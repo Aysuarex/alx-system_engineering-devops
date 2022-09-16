b92b8076f8528a821a8452297ba4c145399846c5
Ayomide Suara <aysuarex@gmail.com>
2022-09-16 13:02:27 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,7 +2,7 @@
 
 package { 'flask':
     ensure => 'present',
-    command => 'pip3 instal flask flask_restful apiai',
+    command => 'pip3 install flask flask_restful apiai',
     path => ['/usr/bin'],
     unless => '/usr/bin/test -f /usr/local/lib/python3.4/dist-packages/flask/app.py'
 }
