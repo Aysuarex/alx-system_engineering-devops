bffc72ec3da09561732ade3ba64bf1e37618279a
Aysuarex <aysuarex@gmail.com>
2022-09-16 13:05:14 +0100

    install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,6 @@
 # Installs flask from pip3 using Puppet
 
-package { 'flask':
-    ensure => 'present',
-    command => 'pip3 instal flask flask_restful apiai',
-    path => ['/usr/bin'],
-    unless => '/usr/bin/test -f /usr/local/lib/python3.4/dist-packages/flask/app.py'
-}
+exec { 'install flask':
+  command => 'sudo pip3 install flask==2.1.0',
+  path => '/usr/bin',
+  }
