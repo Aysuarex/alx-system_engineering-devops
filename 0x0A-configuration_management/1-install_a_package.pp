80c1363a7e48abbfed7513c14caa541fcea44842
Ayomide Suara <aysuarex@gmail.com>
2022-09-17 01:22:06 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# Installs flask from pip3 using Puppet
-
-exec { 'install flask':
-  command => 'sudo pip3 install flask -v 2.1.0',
-  path    => '/usr/bin',
-  }
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
+}
