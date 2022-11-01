5908e6f14197efdc59ec11ce1bc87b64bc75470a
Aysuarex <aysuarex@gmail.com>
2022-11-01 22:27:14 +0100

    main.py test file for task 1

---
+++ 0x16-api_advanced/tests/1-main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+1-main
+"""
+import sys
+
+if __name__ == '__main__':
+    top_ten = __import__('1-top_ten').top_ten
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        top_ten(sys.argv[1])
