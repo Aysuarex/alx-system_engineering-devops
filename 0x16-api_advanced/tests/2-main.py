e685bfe046e2935aff5ee7116ae9969d46ef9a3d
Aysuarex <aysuarex@gmail.com>
2022-11-01 22:27:21 +0100

    main.py test file for task 2

---
+++ 0x16-api_advanced/tests/2-main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+2-main
+"""
+import sys
+
+if __name__ == '__main__':
+    recurse = __import__('2-recurse').recurse
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        result = recurse(sys.argv[1])
+        if result is not None:
+            print(len(result))
+        else:
+            print("None")
